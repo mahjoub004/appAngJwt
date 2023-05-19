acd3669a172270cd5cf78a0aab7328f4f23451b7
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-05-19 17:21:52 +0200

    Routage de nos pages admin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdmLayoutComponent } from './adm-layout/adm-layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: AdmLayoutComponent, children:[
+
+      {path: '', redirectTo: 'dashboard' , pathMatch:'full'},
+      {path: 'dashboard', component: DashboardComponent},
+      {
+        path: 'user', loadChildren:() => import('./user/user.module')
+        .then(m => m.UserModule)
+      },
+      {
+        path: 'cocktail', loadChildren:() => import('./cocktail/cocktail.module')
+        .then(m => m.CocktailModule)
+      }
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
